fb2d0f030d3e9038370f50a74e4dee0706273ef5
Felix Isensee <f.isensee@gmail.com>
2015-10-23 14:19:49 +0000

    Adding Powercontrol

--- config/routes.rb
+++ config/routes.rb
@@ -19,9 +19,9 @@ ForemanProxmox::Engine.routes.draw do
         constraints(:id => /[^\/]+/) do
             get 'create_vm/:id', :on => :collection, :to => 'virtualmachines#create_vm'
             get 'delete_vm/:id', :on => :collection, :to => 'virtualmachines#delete_vm'
-            get 'start/:id', :on => :collection, :to => 'virtualmachines#start'
-            get 'stop/:id', :on => :collection, :to => 'virtualmachines#stop'
-            get 'reboot/:id', :on => :collection, :to => 'virtualmachines#reboot'
+            get 'start/:id', :on => :collection, :to => 'virtualmachines#start_vm'
+            get 'stop/:id', :on => :collection, :to => 'virtualmachines#stop_vm'
+            get 'reboot/:id', :on => :collection, :to => 'virtualmachines#reboot_vm'
         end
     end
 end
