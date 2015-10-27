2d87c78de57160cb51a86c98bce1c2ef7faf7542
Felix Isensee <f.isensee@gmail.com>
2015-10-27 10:01:25 +0000

    Adding Server Overview

--- config/routes.rb
+++ config/routes.rb
@@ -5,17 +5,15 @@ end
 
 ForemanProxmox::Engine.routes.draw do
     
-    get 'proxmoxserver/start_all_vms' => 'proxmoxservers#start_all_vms', as: 'startall'
-    get 'proxmoxserver/stop_all_vms' => 'proxmoxservers#stop_all_vms', as: 'stopall'
-    get 'proxmoxserver/reboot' => 'proxmoxservers#reboot', as: 'reboot'
-    get 'proxmoxserver/shutdown' => 'proxmoxservers#shutdown', as: 'shutdown'
+    get 'proxmoxservers/start_all_vms' => 'proxmoxservers#start_all_vms', as: 'startall'
+    get 'proxmoxservers/stop_all_vms' => 'proxmoxservers#stop_all_vms', as: 'stopall'
+    get 'proxmoxservers/reboot' => 'proxmoxservers#reboot', as: 'reboot'
+    get 'proxmoxservers/shutdown' => 'proxmoxservers#shutdown', as: 'shutdown'
     
-    get 'proxmoxserver/start_all_vms' => 'proxmoxservers#start_all_vms', as: 'startall'
-    get 'proxmoxserver/start_all_vms' => 'proxmoxservers#start_all_vms', as: 'startall'
+    get 'proxmoxservers/start_all_vms' => 'proxmoxservers#start_all_vms', as: 'startall'
+    get 'proxmoxservers/start_all_vms' => 'proxmoxservers#start_all_vms', as: 'startall'
     
-    resources :proxmoxservers do
-        get ':id/setactive' => 'proxmoxservers#setactive', as: 'setactive'
-    end
+    resources :proxmoxservers
          
     resources :virtualmachines, :only => [] do
         constraints(:id => /[^\/]+/) do
