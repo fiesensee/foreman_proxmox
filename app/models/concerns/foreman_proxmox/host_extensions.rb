c4b1a3ec6e788c2a1c0f244296addf66958bb5c7
Felix Isensee <f.isensee@gmail.com>
2015-11-03 14:57:37 +0000

    Finishing v 0.1

--- app/models/concerns/foreman_proxmox/host_extensions.rb
+++ app/models/concerns/foreman_proxmox/host_extensions.rb
@@ -12,6 +12,27 @@ module ForemanProxmox
         vm.delete_virtualmachine
         super
     end
+    
+    def create
+      new_vm = Virtualmachine.new
+      if self.params['vmid'] == nil then
+        new_vm.get_free_vmid
+      else
+          new_vm.vmid = self.params['vmid']
+      end
+        new_vm.sockets = self.params['sockets']
+        new_vm.cores = self.params['cores']
+        new_vm.memory = self.params['memory']
+        new_vm.size = self.params['size']
+        new_vm.mac = self.mac
+        new_vm.host_id = self.id
+        new_vm.save
+        
+        new_vm.create_harddisk
+        new_vm.create_virtualmachine
+        new_vm.start
+        super
+    end
 
     module ClassMethods
       # create or overwrite class methods...
