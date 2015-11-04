4f2a8ab351156ea00273e671fde3ba5b972cdc74
Felix Isensee <f.isensee@gmail.com>
2015-11-04 10:54:47 +0000

    Finishing v 1.0

--- app/models/foreman_proxmox/virtualmachine.rb
+++ app/models/foreman_proxmox/virtualmachine.rb
@@ -3,39 +3,39 @@ module ForemanProxmox
     belongs_to :host
     
     def create_harddisk
-      proxmoxserver = Proxmoxserver.where("current = 'true'").first
+      proxmoxserver = Proxmoxserver.where(:current => true).first
       proxmoxserver.create_ide(self.vmid,self.size)
     end
     
     def create_virtualmachine
-      proxmoxserver = Proxmoxserver.where("current = 'true'").first
+      proxmoxserver = Proxmoxserver.where(:current => true).first
       proxmoxserver.create_kvm(self.vmid,self.sockets,self.cores,self.memory,self.mac)
     end
     
     def start
-      proxmoxserver = Proxmoxserver.where("current = 'true'").first
+      proxmoxserver = Proxmoxserver.where(:current => true).first
       proxmoxserver.start_kvm(self.vmid)
     end
     
     def stop
-      proxmoxserver = Proxmoxserver.where("current = 'true'").first
+      proxmoxserver = Proxmoxserver.where(:current => true).first
       proxmoxserver.stop_kvm(self.vmid)
     end
     
     def reboot
-      proxmoxserver = Proxmoxserver.where("current = 'true'").first
+      proxmoxserver = Proxmoxserver.where(:current => true).first
       proxmoxserver.reboot_kvm(self.vmid)
     end
     
     def delete_virtualmachine
-      proxmoxserver = Proxmoxserver.where("current = 'true'").first
+      proxmoxserver = Proxmoxserver.where(:current => true).first
       self.stop
       proxmoxserver.delete_kvm(self.vmid)
       self.destroy
     end
     
     def get_free_vmid
-      proxmoxserver = Proxmoxserver.where("current = 'true'").first
+      proxmoxserver = Proxmoxserver.where(:current => true).first
       self.vmid = proxmoxserver.get_next_free_vmid
     end
   end
