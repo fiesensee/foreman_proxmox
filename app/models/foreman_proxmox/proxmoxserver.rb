388ab2e8b98e078c72e90504fd58ca50e2b6a729
Felix Isensee <f.isensee@gmail.com>
2015-11-10 09:23:18 +0000

    Adding Server Extras

--- app/models/foreman_proxmox/proxmoxserver.rb
+++ app/models/foreman_proxmox/proxmoxserver.rb
@@ -138,6 +138,9 @@ module ForemanProxmox
         return nil
       end
       authenticate_client
+      if name == nil
+        name = vmid
+      end
       body= { :vmid => vmid, :name => name, :sockets => sockets, :cores => cores, :memory => memory, :net0 => "e1000=#{mac},bridge=#{self.bridge}", :ide0 => "volume=#{self.storage}:#{vmid}/vm-#{vmid}-disk-0.qcow2,media=disk"}
       testres= @client.post("https://#{self.ip}:8006/api2/json/nodes/#{@node}/qemu",body,@header)
       $LOG.error("Body: #{testres.body}")
