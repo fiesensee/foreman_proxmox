1a4c01161c2e55af8a744fb8e99d40d71f1a85c2
Felix Isensee <f.isensee@gmail.com>
2015-12-15 11:46:07 +0000

    override

--- app/controllers/concerns/foreman_proxmox/subnets_controller_extensions.rb
+++ app/controllers/concerns/foreman_proxmox/subnets_controller_extensions.rb
@@ -6,7 +6,7 @@ module ForemanProxmox
     end
     
     def freeip
-    #$LOG = Logger.new("/tmp/mac.log")
+    $LOG = Logger.new("/tmp/mac.log")
     invalid_request and return unless (s=params[:subnet_id].to_i) > 0
     organization = params[:organization_id].blank? ? nil : Organization.find(params[:organization_id])
     location = params[:location_id].blank? ? nil : Location.find(params[:location_id])
@@ -14,9 +14,9 @@ module ForemanProxmox
       not_found and return unless (subnet = Subnet.authorized(:view_subnets).find(s))
       if (ip = subnet.unused_ip(params[:host_mac], params[:taken_ips]))
 		ip_array = ip.split(".")
-		#$LOG.error("Before mac")
+		$LOG.error("Before mac")
 		mac = "00:50:#{ip_array[0].to_i.to_s(16)}:#{ip_array[1].to_i.to_s(16)}:#{ip_array[2].to_i.to_s(16)}:#{ip_array[3].to_i.to_s(16)}"
-		#$LOG.error("MAC: #{mac}")
+		$LOG.error("MAC: #{mac}")
         render :json => {:ip => ip, :mac => mac}
       else
         not_found
