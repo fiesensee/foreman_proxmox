a7f2ee3eb727bcd6c0fa744147843d90011ef177
Felix Isensee <f.isensee@gmail.com>
2015-12-15 13:45:36 +0000

    override

--- MainAppEdits/override.sh
+++ MainAppEdits/override.sh
@@ -2,4 +2,4 @@
 
 cp host_edit.js /usr/share/foreman/app/assets/javascripts/host_edit.js
 cp host_edit-0e432be6b349e7084c604a319b390e2e.js /usr/share/foreman/public/assets/host_edit-0e432be6b349e7084c604a319b390e2e.js
-cp subnets_controller.rb /usr/share/foreman/app/controller/subnets_controller.rb
+cp subnets_controller.rb /usr/share/foreman/app/controllers/subnets_controller.rb
