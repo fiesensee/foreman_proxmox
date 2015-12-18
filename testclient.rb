3a371fe60a47a9319642edf0bc7c3f7994ef5ad2
Felix Isensee <f.isensee@gmail.com>
2015-12-18 13:35:09 +0000

    status&error

--- testclient.rb
+++
@@ -1,8 +0,0 @@
-require 'httpclient'
-class Testclient
-    client = HTTPClient.new
-    client.ssl_config.verify_mode= OpenSSL::SSL::VERIFY_NONE
-    #client.get("https://192.168.37.149:8006/api2/json/access/ticket")
-    testres = client.get("http://github.com")
-    puts testres.header.reason_phrase
-end
